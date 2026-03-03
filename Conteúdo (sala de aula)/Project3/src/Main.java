7baceeb5bcd35f6aea36a2f37950f1e5d8d195c0
PedroEuzebio06 <pedro.bananal06@gmail.com>
2026-03-03 19:59:02 -0300

    Add files via upload

---
+++ Conteúdo (sala de aula)/Project3/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Instanciar um objeto da classe Scanner
+		Scanner teclado;
+		teclado = new Scanner(System.in);
+		// Scanner teclado = new Scanner (System.in);
+		
+		// declarar variaveis
+		
+		int numero1, numero2, soma;
+		System.out.println("Digite um número inteiro: ");
+		numero1 = teclado.nextInt();
+		System.out.println("Digite outro número: ");
+		numero2 = teclado.nextInt();
+		
+		soma = numero1 + numero2;
+		
+		System.out.println("Soma = " + soma);
+		
+		//fechar o teclado
+		teclado.close();
+
+	}
+
+}
