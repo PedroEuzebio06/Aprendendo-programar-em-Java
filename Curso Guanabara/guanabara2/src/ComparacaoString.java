961ad524368901ef89d8035cd2b8b2bed5e87257
PedroEuzebio06 <pedro.bananal06@gmail.com>
2026-03-04 16:32:14 -0300

    Add files via upload

---
+++ Curso Guanabara/guanabara2/src/ComparacaoString.java
@@ -0,0 +1,27 @@
+
+public class ComparacaoString {
+
+	public static void main(String[] args) {
+		
+//		Compara o conteúdo
+//		String nome1 = "Pedro";
+//		String nome2 = "Pedro";
+//		String nome3 = new String ("Pedro");
+//		String res;
+//		res = (nome1.equals(nome3))?"iguais":"diferentes";
+//		System.out.println(res);
+		
+		
+//		String nome1 = "Pedro";
+//		String nome2 = "Pedro";
+//		String nome3 = new String ("Pedro");
+//		String res;
+//		res = (nome1 == nome3)?"iguais":"diferentes";
+//		System.out.println(res);
+		
+		
+		
+
+	}
+
+}
---
+++ Curso Guanabara/guanabara2/src/OperadorTernario.java
@@ -0,0 +1,16 @@
+
+public class OperadorTernario {
+
+	public static void main (String[] args) {
+		
+//		Mostrar valor maior
+//		int n1, n2, r;
+//		n1 = 14;
+//		n2 = 28;
+//		r = (n1>n2)?n1:n2;
+//		System.out.println(r);
+		
+		 
+
+	}
+}
